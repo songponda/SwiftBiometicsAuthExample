0690d0cefdecfcc9db5fd2dccdde7e48e434a394
songpoda <songpoda@tlt.co.th>
2023-02-14 13:06:52 +0700

    Initial Commit

---
+++ BiometricAuthExample/BiometricAuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthExampleApp.swift
+//  BiometricAuthExample
+//
+//  Created by songpon_tlt on 14/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuthExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BiometricAuthExample
+//
+//  Created by songpon_tlt on 14/2/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
